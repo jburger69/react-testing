9a3c9635ee2c33c6a5ba1804af828bbdca554131
Joshua Burger <joshberger6969@outlook.com>
2021-12-26 21:25:56 -0500

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Increase The Count or Decrease</h1>
+        </div>
+    )
+}
+
+export default Header;
