b65b3c471dba4592ca28afc28fcafa22f934444f
Joshua Burger <joshberger6969@outlook.com>
2021-12-26 21:25:24 -0500

    create header component, increase button, decrease

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import {useState} from 'react';
+import Header from './components/Header';
+import Decreasebutton from './components/DecreaseButton';
+import Increasebutton from './components/IncreaseButton';
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <h1>You Clicked {count} times!</h1>
+      <Increasebutton count={count} setCount={setCount}/>
+      <Decreasebutton count={count} setCount={setCount}/>
     </div>
   );
 }
